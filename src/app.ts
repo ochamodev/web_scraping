a864f94c0ebe72a7468bf76024577aecd6d442a4
ochamodev <cheleyotto98@gmail.com>
2024-07-04 22:22:19 -0600

    chore: Initial commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import { chromium } from "playwright";
+import { SimanProduct } from "./core/model/simanProduct";
+
+let simanProduct = new SimanProduct("", "")
+
+simanProduct.
---
+++ src/core/model/simanProduct.ts
@@ -0,0 +1,12 @@
+
+export class SimanProduct {
+    name: String;
+    detailLink: String;
+    constructor(
+        name: string,
+        detailLink: string,
+    ) {
+        this.name = name;
+        this.detailLink = detailLink;
+    }
+}
